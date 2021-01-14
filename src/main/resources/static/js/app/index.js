fd9f7d7f1da8e972a72c3dbfa1cde03e49bf9660
OGAREE <ohkangun@naver.com>
2021-01-14 15:37:57 +0900

    게시글 삭제 화면 만들기 157쪽

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,7 +3,9 @@ var main = {
         var _this = this;
         $('#btn-save').on('click', function() {_this.save();
         });
-        $('#btn-update').on('click', function () {_this.update();
+        $('#btn-update').on('click', function() {_this.update();
+        });
+        $('#btn-delete').on('click', function() {_this.delete();
         });
     },
     save : function(){
@@ -43,6 +45,21 @@ var main = {
         }).done(function(){
             alert('글이수정되었습니다.');
             window.location.href = '/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href='/';
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
